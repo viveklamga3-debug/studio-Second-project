437d8b92a34061f25329caeff258c530651f38dd
Vivek lamga <viveklamga3@gmail.com>
2025-12-04 04:53:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5288681870-8c231",
+  "appId": "1:32331534924:web:4587a88ea5d735afd8dae9",
+  "apiKey": "AIzaSyAyOYwmqRsTDsItJRxCTOdho4yQA3utQss",
+  "authDomain": "studio-5288681870-8c231.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "32331534924"
+};
