50b97bcfa57ab287206142c5dd9832bfa0366308
viveklamga3-debug <viveklamga3@gmail.com>
2025-12-05 11:39:37 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,14 +19,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-CBKLFW2W5E"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-CBKLFW2W5E');
-</script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
