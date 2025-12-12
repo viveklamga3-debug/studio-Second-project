e44146e08bbb7bda0ef057d362710cff0f49deae
viveklamga3-debug <viveklamga3@gmail.com>
2025-12-12 11:10:30 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-5288681870-8c231",
-  "appId": "1:32331534924:web:4587a88ea5d735afd8dae9",
-  "apiKey": "AIzaSyAyOYwmqRsTDsItJRxCTOdho4yQA3utQss",
-  "authDomain": "studio-5288681870-8c231.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "32331534924"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
